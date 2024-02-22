19f7baf2f5fe8b14fecf99466552635013fa3d76
NavyBoy37 <joshuayawn.epworth@gmail.com>
2024-02-21 23:54:22 -0500

    first commit

---
+++ linked_list.py
@@ -0,0 +1,9 @@
+"""A normal list in python is an array that uses indexed storage.
+ This means it can call upon individual elements of that array without
+ cycling through all of them."""
+
+"""A linked list cannot call upon individual components of its storage.
+Rather it cycles through the list in a direction based off address nodes which
+are placed with the actual information.  It cycles through until it gets what 
+it needs.  It is like a FOR loop with the array list.  It checks each obj in the
+list until it finds what it needs."""
